2a6c781ef331dd25e1751eeef1e6b441befe609d
krimmmmm <krimmmmm@gmail.com>
2026-05-29 17:14:42 +0700

    Create input.tsx

---
+++ client/src/components/ui/input.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
