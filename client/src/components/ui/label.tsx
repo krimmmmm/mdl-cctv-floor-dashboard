9c4649c56dffa655b0683e8c7596c560c03b99f6
krimmmmm <krimmmmm@gmail.com>
2026-05-29 16:45:25 +0700

    Create label.tsx

---
+++ client/src/components/ui/label.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Label({
+  className = "",
+  children,
+  ...props
+}: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <label
+      className={`text-sm font-medium leading-none ${className}`}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+}
