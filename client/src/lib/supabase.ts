2be88aee113bac5c0ca6eda944c942ef35187d5b
krimmmmm <krimmmmm@gmail.com>
2026-05-30 14:17:09 +0700

    Update supabase.ts

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://xsrilpwdbqwhqsgsjjxh.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_Eq6NXbT-vIj-msgvAE9ZUw_h-LXyfGi";
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  supabaseUrl,
+  supabaseKey
 );
+update supabase config
