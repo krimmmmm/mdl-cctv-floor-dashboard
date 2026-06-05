8133fe7616a6cb2908cb015f599e47b3cbc209ef
krimmmmm <krimmmmm@gmail.com>
2026-06-05 12:54:07 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -44,7 +44,7 @@ function Router() {
       <ProtectedRoute
         path="/schedule"
         component={SchedulePage}
-        allowedRoles={["admin", "newstaff", "staffonly", "staff", "customer"]}
+        allowedRoles={["admin"]}
       />
 
       <ProtectedRoute
